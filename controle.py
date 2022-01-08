3570dcf5b2f17ca056cef94fab90e6d37e97d127
Andre R M Conforti <andre.r.m.conforti@gmail.com>
2022-01-08 17:37:22 -0300

    Formulário de consulta
    Criado o formulário para consulta dos produtos e conectando ao banco de dados, é possível visualizar os dados já no formulário. Porém, é preciso formatar a tabela.

--- controle.py
+++ controle.py
@@ -20,23 +20,43 @@ def funcao_principal():
     else:
         categoria = 'Limpeza'
     
-    print('teste')
     print('Codigo', linha1)
     print('Descrição', linha2)
     print('Preço', linha3)
+    print('Categoria', categoria)
 
     cursor = banco.cursor()
     comando_SQL = 'INSERT INTO produtos (codigo, descricao, preco, categoria) VALUES (%s, %s, %s, %s)'
     dados = (str(linha1), str(linha2), str(linha3), categoria)
     cursor.execute(comando_SQL, dados)
     banco.commit()
+    formulario.codigo.setText('')
+    formulario.descricao.setText('')
+    formulario.preco.setText('')
 
 
+def consulta_produtos():
+    consulta.show()
+
+    cursor = banco.cursor()
+    comando_SQL = 'SELECT * FROM produtos'
+    cursor.execute(comando_SQL)
+    dados_lidos = cursor.fetchall()
+    print(dados_lidos[1])
+
+    consulta.tabela.setRowCount(len(dados_lidos))
+    consulta.tabela.setColumnCount(5)
+    
+    for i in range(0, len(dados_lidos)):
+        for j in range(0, 5):
+            consulta.tabela.setItem(i, j, QtWidgets.QTableWidgetItem(str(dados_lidos[i][j])))
 
 
 app = QtWidgets.QApplication([])
 formulario = uic.loadUi('formulario.ui')
+consulta = uic.loadUi('consulta.ui')
 formulario.cadastrar.clicked.connect(funcao_principal)
+formulario.consultar.clicked.connect(consulta_produtos)
 
 formulario.show()
 app.exec()
